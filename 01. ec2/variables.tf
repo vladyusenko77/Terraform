75e96d7cdd30d8d9431a55fab3899d55e4ac2056
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-07-01 17:09:56 +0300

    ec2 and vpc

--- 01. ec2/variables.tf
+++ 01. ec2/variables.tf
@@ -1,8 +1,8 @@
 variable aws_access_key { 
     type = string
-    default = "AKIAZOVODH3YY3ZAPGXX" 
+    default = "aws_access_key" 
 }
 variable aws_secret_key { 
     type = string
-    default = "QugJaGjmRKNAJ1jwedmbki4aIjfG+m3dGYwwb5l8" 
+    default = "aws_secret_key" 
 }
--- 02. vpc/variables.tf
+++ 02. vpc/variables.tf
@@ -1,10 +1,10 @@
 variable aws_access_key { 
     type = string
-    default = "AKIAZOVODH3YY3ZAPGXX" 
+    default = "aws_access_key" 
 }
 variable aws_secret_key { 
     type = string
-    default = "QugJaGjmRKNAJ1jwedmbki4aIjfG+m3dGYwwb5l8" 
+    default = "aws_secret_key" 
 }
 
 variable subnet_count{
